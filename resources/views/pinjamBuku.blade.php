2cb72e217553c09dac1e579e3e667212fe967bb5
My name <eillenhartono@gmail.com>
2022-12-18 21:01:46 +0700

    add: book

--- resources/views/pinjamBuku.blade.php
+++ resources/views/pinjamBuku.blade.php
@@ -31,7 +31,7 @@
 
     <div class="form-group">
         <div class="col-sm-offset-2 col-sm-10">
-            <button type="submit" class="btn btn-default">Simpan</button>
+            <button type="submit" class="btn btn-default">Pinjam</button>
         </div>
     </div>
 </form>
